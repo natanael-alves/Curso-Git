af93e3a34d8e237c59dc25f9df53b36373577967
natanael-alves <natan.alves.silva16@gmail.com>
2025-11-06 14:10:02 -0400

    Tipos de variáveis

---
+++ variaveis.py
@@ -0,0 +1,16 @@
+nome = "Natanael Alves"  # variável do tipo string (texto), sempre entre aspas ("" ou '').
+idade = 32               # var do tipo inteiro (número sem casas decimais).
+altura = 1.73            # var do tipo float (número com casas decimais).
+dev = True               # var do tipo booleana, valores lógicos (true/falso).
+
+#print(f"Olá, {nome}! Você tem {idade} anos e mede {altura}m.")
+
+
+
+
+
+nome = input("Digite seu nome: ")
+idade = int(input("Digite sua idade: "))
+altura = float(input("Digite sua altura: "))
+
+print(f"Olá, {nome} ! Você tem {idade} anos e mede {altura}m.")
