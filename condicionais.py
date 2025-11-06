6ca6f8af6fee495545980992b71328cb1c663cff
natanael-alves <natan.alves.silva16@gmail.com>
2025-11-06 13:47:10 -0400

    Estudo sobre if/else e solicitando informações do usuário.

---
+++ condicionais.py
@@ -0,0 +1,16 @@
+#idade = int(input("Digite sua idade: "))
+
+#if idade >=18:
+    #print("Você pode entrar no evento")
+#else:
+    #print("Desculpe, você ainda não tem idade suficiente para entrar. ")
+   
+
+
+grana = float(input("Digite a quantidade de grana que você tem: "))
+if grana >= 20:
+    print("Opá, você pode tomar uma café ! !")
+elif grana >= 10:
+    print("Você tomara somente o café.")
+else:
+    print("Nem entre na cafeteria, vá sim bora...")
