4914bb715e0191afb2a7f31c8511c5d030f1b2a9
natanael-alves <natan.alves.silva16@gmail.com>
2025-11-07 21:35:49 -0400

    Repetição for/while

---
+++ repeticao.py
@@ -0,0 +1,14 @@
+#for  numero in range(1, 101):
+    #print(numero)
+    
+#compras = ["Arroz" , "Feijão" , "Açucar" , "Macarrão" , "Ovos"]
+
+#for item in compras:
+   #print(f"compras: {item}")
+
+#palavra = "Python"
+
+#for letra in palavra:
+ #   print(letra)
+
+
