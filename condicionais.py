87517b08e90c9e31aa15ed7b8c7e218594dd33f3
natanael-alves <natan.alves.silva16@gmail.com>
2025-11-25 21:51:32 -0400

    Exercício com if, elif e else

--- condicionais.py
+++ condicionais.py
@@ -4,7 +4,7 @@
     #print("Você pode entrar no evento")
 #else:
     #print("Desculpe, você ainda não tem idade suficiente para entrar. ")
-   
+
 
 
 grana = float(input("Digite a quantidade de grana que você tem: "))
@@ -13,4 +13,4 @@ if grana >= 20:
 elif grana >= 10:
     print("Você tomara somente o café.")
 else:
-    print("Nem entre na cafeteria, vá sim bora..")
+    print("Nem entre na cafeteria, vá sim bora...")
